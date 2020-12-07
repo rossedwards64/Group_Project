48c8cf339c1f353ed52ffca884818fca4da7082a
Ross Edwards <redwards64@hotmail.com>
2020-12-07 15:24:23 +0000

    Add project files.

---
+++ Group_Project/Pages/AdminPages/ViewUsers.cshtml.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Group_Project.Models;
+using Group_Project.Pages.DatabaseConnection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Group_Project.Pages.AdminPages
+{
+    public class ViewUsersModel : PageModel
+    {
+        [BindProperty]
+        public List<User> User { get; set; }
+
+        public List<string> URole { get; set; } = new List<string> { "User", "Admin" };
+        public string UserName;
+        public const string SessionKeyName1 = "username";
+
+
+        public string FirstName;
+        public const string SessionKeyName2 = "fname";
+
+        public string SessionID;
+        public const string SessionKeyName3 = "sessionID";
+        public IActionResult OnGet()
+        {
+            //get the session first!
+            UserName = HttpContext.Session.GetString(SessionKeyName1);
+            FirstName = HttpContext.Session.GetString(SessionKeyName2);
+            SessionID = HttpContext.Session.GetString(SessionKeyName3);
+
+
+            DatabaseConnect dbstring = new DatabaseConnect(); //creating an object from the class
+            string DbConnection = dbstring.DatabaseString(); //calling the method from the class
+            Console.WriteLine(DbConnection);
+            SqlConnection conn = new SqlConnection(DbConnection);
+            conn.Open();
+
+            using (SqlCommand command = new SqlCommand())
+            {
+                command.Connection = conn;
+                command.CommandText = @"SELECT * FROM UserTable";
+
+                var reader = command.ExecuteReader();
+
+                User = new List<User>();
+                while (reader.Read())
+                {
+                    User Row = new User(); //each record found from the table
+                    Row.Id = reader.GetInt32(0);
+                    Row.FirstName = reader.GetString(1);
+                    Row.UserName = reader.GetString(2);
+                    Row.Role = reader.GetString(4); // We dont get the password. The role field is in the 5th position
+                    User.Add(Row);
+                }
+                
+            }
+            return Page();
+
+        }
+    }
+}
---
+++ Group_Project/Pages/DatabaseConnection/DatabaseConnect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group_Project.Pages.DatabaseConnection
+{
+    public class DatabaseConnect
+    {
+        public string DatabaseString ()
+        {
+            string DbString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C: \Users\redwa\Documents\Uni Work\Web\C# Module\Group Project\Group_Project\Group_Project\Data\Login_Session.mdf;Integrated Security=True;Connect Timeout=30";
+            return DbString;
+        }
+    }
+}
