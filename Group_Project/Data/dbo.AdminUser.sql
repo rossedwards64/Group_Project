d575534d68bace08e04729126f2859414fa5ded4
Ross Edwards <redwards64@hotmail.com>
2020-12-07 20:51:59 +0000

    added primary keys

---
+++ Group_Project/Data/dbo.AdminUser.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[AdminUser] (
+    [AdminUserID]   INT          IDENTITY (1, 1) NOT NULL,
+    [AdminUsername] VARCHAR (20) NOT NULL,
+    [AdminPassword] VARCHAR (20) NOT NULL, 
+    CONSTRAINT [PK_AdminUser] PRIMARY KEY ([AdminUserID])
+);
+
---
+++ Group_Project/Data/dbo.Files.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Files] (
+    [FileUploader] VARCHAR (MAX) NOT NULL,
+    [FileName]     VARCHAR (MAX) NOT NULL,
+    [Date]         DATE          NOT NULL, 
+    [FileID] INT NOT NULL, 
+    CONSTRAINT [PK_Files] PRIMARY KEY ([FileID])
+);
+
